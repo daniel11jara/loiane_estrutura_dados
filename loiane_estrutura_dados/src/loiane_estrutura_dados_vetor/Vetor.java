ac80cbddac447fdfe11c947fed85cfe472c2c027
Daniel Jara <97138120+daniel11jara@users.noreply.github.com>
2024-03-05 23:04:04 -0300

    aula 03

--- loiane_estrutura_dados/src/loiane_estrutura_dados_vetor/Vetor.java
+++ loiane_estrutura_dados/src/loiane_estrutura_dados_vetor/Vetor.java
@@ -1,11 +1,37 @@
 package loiane_estrutura_dados_vetor;
 
+import java.util.Iterator;
+
 public class Vetor {
 	
+	//aula 02
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+	}
+	
+	//aula 03
+	/*public void adiciona(String elemento) {
+		for (int i = 0; i < this.elementos.length; i++) {
+			//verificando se a posição está nula
+			if(this.elementos[i] == null) {
+				elementos[i] = null;
+				break;
+			}
+		}
+	}*/
+	
+	//código mais eficiente
+	public void adiciona(String elemento) {
+		//verificando se o tamanho é menor que a capacidade do vetor elementos
+		
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+		}
+		
+		
 	}
 
 }
---
+++ loiane_estrutura_dados/src/loiane_estrutura_dados_vetor_teste/Aula03.java
@@ -0,0 +1,19 @@
+package loiane_estrutura_dados_vetor_teste;
+
+import loiane_estrutura_dados_vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vetor vetor = new Vetor(2);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		
+		
+
+	}
+
+}
